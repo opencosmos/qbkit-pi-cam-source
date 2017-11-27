4e964d31ee3228421a0886c594ad2cc24f8f859f
Mark K Cowan <mark@battlesnake.co.uk>
2017-11-27 18:23:17 +0000

    power: power-on timeout increased to 90s for pi zero with stock configuration

--- payload/power.h
+++ payload/power.h
@@ -3,7 +3,7 @@
 #include "../power/power.h"
 #include "../uart/proto.h"
 
-#define PAYLOAD_POWER_ON_TIMEOUT_S 60
+#define PAYLOAD_POWER_ON_TIMEOUT_S 90
 #define PAYLOAD_POWER_OFF_TIMEOUT_S 60
 #define PAYLOAD_WARM_UP_TIME_S 10
 #define PAYLOAD_COOL_DOWN_TIME_S 10
