51402cb7af64cf1bc2dcead5a5979c5eb441cfeb
Mark K Cowan <mark@battlesnake.co.uk>
2017-11-27 18:19:13 +0000

    power: use POSIX IO to access power lines

--- power/power.c
+++ power/power.c
@@ -7,22 +7,20 @@ bool payload_power_set(const char *line, bool state)
 	bool ret = false;
 	char path[50];
 	snprintf(path, sizeof(path), fmt, line);
-	FILE *f = fopen(path, "w");
-	if (!f) {
+	int fd = open(path, O_WRONLY | O_SYNC);
+	if (fd < 0) {
 		logsysfail("Failed to open power line");
 		goto fail;
 	}
-	if (fwrite(state ? "1" : "0", 1, 1, f) != 1) {
+	if (write(fd, state ? "1" : "0", 1) != 1) {
 		logsysfail("Failed to set power state");
 		goto fail1;
 	}
 	ret = true;
-	goto done;
 fail1:
-	if (fclose(f)) {
+	if (close(fd)) {
 		logsysfail("Failed to close power line");
 	}
 fail:
-done:
 	return ret;
 }
